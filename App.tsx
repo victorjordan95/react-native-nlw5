49529b77f4b6a87664d4192d7a8512c12e29adde
Victor Jordan <victorjordan@fitec.org.br>
2021-04-21 15:36:28 -0300

    Day #02 - Finished

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { Jost_400Regular, Jost_600SemiBold, useFonts } from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading';
+import React from 'react';
+import Routes from './src/routes';
+
+
+export default function App() {
+    const [fontsLoaded] = useFonts({
+        Jost_400Regular,
+        Jost_600SemiBold
+    })
+
+    if(!fontsLoaded) {
+        <AppLoading />
+    }
+    return (
+        <Routes />
+    )
+}
---
+++ src/pages/Confirmation/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, Dimensions, View} from 'react-native';
+import wateringImg from '../../assets/watering.png';
+import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
+import { Feather } from '@expo/vector-icons'
+import { Button } from '../../components/Button';
+
+export function Confirmation() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.emoji}>
+          😄
+        </Text>
+        <Text style={styles.title}>
+          Prontinho
+        </Text>
+        <Text style={styles.subtitle}>
+          Agora vamos começar a cuidar das suas plantinhas com muito cuidado
+        </Text>
+        <View style={styles.footer}>
+          <Button title="Começar" />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%'
+  },
+  emoji: {
+    fontSize: 78,
+  },
+  title: {
+    fontSize: 22,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 38,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    textAlign: 'center',
+    fontSize: 17,
+    paddingVertical: 10,
+    color: colors.heading,
+  },
+  footer: {
+    width: '100%',
+    paddingHorizontal: 50,
+    marginTop: 20
+  }
+})
