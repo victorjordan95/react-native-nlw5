235468d8bf3e3efccee1cce50c6db683731f2259
Victor Jordan <victorjordan@fitec.org.br>
2021-04-21 21:30:30 -0300

    Day #03 - Finished

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
-import { Jost_400Regular, Jost_600SemiBold, useFonts } from '@expo-google-fonts/jost';
-import AppLoading from 'expo-app-loading';
 import React from 'react';
-import Routes from './src/routes';
+import AppLoading from 'expo-app-loading';
 
+import Routes from './src/routes';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
 
 export default function App() {
-    const [fontsLoaded] = useFonts({
-        Jost_400Regular,
-        Jost_600SemiBold
-    })
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
 
-    if(!fontsLoaded) {
-        <AppLoading />
-    }
-    return (
-        <Routes />
-    )
+  if (!fontsLoaded)
+    return <AppLoading />
+
+  return (
+    <Routes />
+  )
 }
--- src/pages/Confirmation/index.tsx
+++ src/pages/Confirmation/index.tsx
@@ -5,8 +5,16 @@ import colors from '../../styles/colors';
 import fonts from '../../styles/fonts';
 import { Feather } from '@expo/vector-icons'
 import { Button } from '../../components/Button';
+import { useNavigation } from '@react-navigation/core';
 
 export function Confirmation() {
+  const navigation = useNavigation();
+
+
+  function handleMoveOn() {
+    navigation.navigate('PlantSelect');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -20,7 +28,7 @@ export function Confirmation() {
           Agora vamos começar a cuidar das suas plantinhas com muito cuidado
         </Text>
         <View style={styles.footer}>
-          <Button title="Começar" />
+          <Button title="Começar" onPress={handleMoveOn} />
         </View>
       </View>
     </SafeAreaView>
